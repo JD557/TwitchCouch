6f63869e249c864dd3b07b0096d71b91f45089e1
João Costa <jdpc557@gmail.com>
2013-07-11 15:25:18 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 TwitchCouch
 ===========
 
-Simple website to browse twitch.tv streams on a smartTV browser
+Simple website to browse twitch.tv streams on a TV web browser.
+
+What is it?
+-----------
+
+TwitchCouch is a simple website to allow an easier Twitch.tv browsing experience
+in TV web browsers.
+
+Why?
+----
+
+Modern TV web browsers are pretty powerful, although browsing the web with
+a remote control is a painful experience.
+
+This is especially true for Twitch.tv, where most users that wanted to see
+game streams on their would need to manually type the channel url
+(www.twitch.tv/channel_name/popout).
+
+This is a cumbersome task, especially since an user needs to know the channel
+name in advance (and some TVs don't allow you to easily correct typos).
+
+Demo
+----
+
+The project is availible at http://twitchcouch.neocities.org/
---
+++ script.js
@@ -0,0 +1,85 @@
+/* Global Arrays */
+var streams=new Array();
+var games=new Array();
+
+/* Streams */
+function Stream(channel,game,status,logo,viewers) {
+	this.channel=channel;
+	this.game=game;
+	this.status=status;
+	this.logo=logo;
+	this.viewers=viewers;
+}
+
+Stream.prototype.render = function(){
+	return	"<a href='http://twitch.tv/"+this.channel+"/popout'>"+
+			"<img src='"+this.logo+"'><br/>"+
+			this.status +
+			"</a><br/>"+
+			"Playing <a href='javascript:getStreamsByGame(\""+this.game+"\");'>"+this.game+"</a><br/>"+
+			this.viewers+" viewers";
+}
+
+function getMostViewed() {
+	streams=new Array();
+	$("#streamsTitle").html("Most Viewed Streams");
+	$("#streams").html("");
+	Twitch.api({method: 'streams', params: {limit:5} }, function(error, list) {
+		console.log(list);
+		for (x in list.streams) {
+			var channel=list.streams[x].channel;
+			streams.push(new Stream(channel.name,channel.game,channel.status,channel.logo,list.streams[x].viewers));
+		}
+		for (x in streams) {
+			$("#streams").append("<li>"+streams[x].render()+"</li>");
+		}
+	});
+}
+
+function getStreamsByGame(gamename) {
+	streams=new Array();
+	$("#streamsTitle").html(gamename+" Streams");
+	$("#streams").html("");
+	Twitch.api({method: 'streams', params: {limit:5,game:gamename} }, function(error, list) {
+		console.log(list);
+		for (x in list.streams) {
+			var channel=list.streams[x].channel;
+			streams.push(new Stream(channel.name,channel.game,channel.status,channel.logo,list.streams[x].viewers));
+		}
+		for (x in streams) {
+			$("#streams").append("<li>"+streams[x].render()+"</li>");
+		}
+	});
+}
+
+/* Games */
+function Game(name,logo) {
+	this.name=name;
+	this.logo=logo;
+}
+
+Game.prototype.render = function(){
+	return	"<a href='javascript:getStreamsByGame(\""+this.name+"\")'>"+
+			"<img src='"+this.logo+"'/><br/>"+
+			this.name +
+			"</a>";
+}
+
+function getGames() {
+	games=new Array();
+	Twitch.api({method: 'games/top', params: {limit:5} }, function(error, list) {
+		console.log(list);
+		for (x in list.top) {
+			games.push(new Game(list.top[x].game.name,list.top[x].game.logo.medium));
+		}
+		for (x in games) {
+			$("#filters ul").append("<li>"+games[x].render()+"</li>");
+		}
+	});
+}
+
+/* Init */
+Twitch.init({clientId: 'YOUR_CLIENT_ID_HERE'}, function(error, status) {
+	getGames();
+	getMostViewed();
+});
